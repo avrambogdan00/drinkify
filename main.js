674900046e90e943c93e421102fb401237c2b1b9
avrambogdan00 <53436932+avrambogdan00@users.noreply.github.com>
2019-07-29 16:48:13 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,54 @@
+//index.html
+
+var baseExist = false;
+var ing = [0, 0, 0, 0];
+
+
+function addImg(value, type, id) {
+  var span = document.getElementsByTagName('span');
+  var img = document.createElement("img");
+  img.src = value;
+  if(type == 0) {
+    if (baseExist == false) {
+      document.getElementById('b').appendChild(img);
+      document.getElementsByName('b')[0].value = id;
+    }
+    baseExist = true;
+  } else if (type == 1) {
+    for(var i=0; i<ing.length; i++) {
+      if(ing[i] == 0) {
+        ing[i] = id;
+        var src = document.getElementById('i'+(i+1));
+        src.appendChild(img);
+
+        var input = document.getElementsByName('i'+(i+1));
+        input[0].value = id;
+        console.log(id);
+
+        break;
+      }
+    }
+  }
+}
+
+
+function alc() {
+  var cbox = document.getElementById('alcohol');
+  var inp = document.getElementById('alc-range');
+  var btn = document.getElementsByClassName('btn');
+  if (cbox.checked == true) {
+    inp.style.display = 'inline';
+    btn[0].style.display = 'inline';
+  } else{
+    inp.style.display = 'none';
+    btn[0].style.display = 'none';
+  }
+}
+
+function checkforerorrs(){
+  var queryString = decodeURIComponent(window.location.search);
+  queryString = queryString.substring(1);
+  if(queryString == 'e') {
+    document.getElementById('err').style.display = 'block';
+  }
+}
---
+++ submit.js
@@ -0,0 +1,29 @@
+var possibleIng = ['ice', 'lemon', 'coconut', 'banana'];
+
+function getOrder(){
+  var ingredients = '';
+  div = document.getElementsByClassName('order');
+  var queryString = decodeURIComponent(window.location.search);
+  queryString = queryString.substring(1);
+  var queries = queryString.split("&");
+
+  var i1 = queries[0].split('='); ingredients = ((i1[1]=='') ? ingredients : ingredients + possibleIng[i1[1]] + '<br>');
+  var i2 = queries[1].split('='); ingredients = ((i2[1]=='') ? ingredients : ingredients + possibleIng[i2[1]] + '<br>');
+  var i3 = queries[2].split('='); ingredients = ((i3[1]=='') ? ingredients : ingredients + possibleIng[i3[1]] + '<br>');
+  var i4 = queries[3].split('='); ingredients = ((i4[1]=='') ? ingredients : ingredients + possibleIng[i4[1]] + '<br>');
+  var b = queries[4].split('='); b = b[1]; b = ((b==0) ? '' : b);
+  var alc = queries[5].split('='); alc = alc[1]+'%';
+  if (alc != '') {
+    div[0].innerHTML = 'Alcohol: ' + alc;
+  }
+
+  b = ((b==0) ? '' : b);
+  div[0].innerHTML += '<h3 style=\'margin-top: 20px; margin-bottom: 0px;\'>Base:</h3> ' + b + '<h3 style=\'margin-top: 20px; margin-bottom: 0px;\'>Ingredients: </h3>' + ingredients;
+
+  if(b == '') {
+    window.location.href = 'index.html?e';
+  }
+  if(i1[1] == '') {
+    window.location.href = 'index.html?e';
+  }
+}
