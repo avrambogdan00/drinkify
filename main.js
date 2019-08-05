32d8bb6a7e2d3e717f29d47dfe6d3fa3cb7806c9
Avram Bogdan <53436932+avrambogdan00@users.noreply.github.com>
2019-08-05 17:20:24 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,39 +1,23 @@
 //index.html
-window.addEventListener('load', (event) => {
 
-
-  var baseExist = false;
+window.addEventListener('load', function(event) {
+var baseExist = false;
   var ing = [0, 0, 0, 0];
 
+function updateHI(value) {
+  for(var i=0; i<ing.length; i++) {
+      if(ing[i] == 0) {
+        ing[i] = value;
+        var src = document.getElementById('i'+(i+1));
 
-  function addImg(value, type, id) {
-    var span = document.getElementsByTagName('span');
-    var img = document.createElement("img");
-    img.src = value;
-    if(type == 0) {
-      if (baseExist == false) {
-        document.getElementById('b').appendChild(img);
-        document.getElementsByName('b')[0].value = id;
+        var input = document.getElementsByName('i'+(i+1));
+        input[0].value = value;
+        console.log(value); 
+
+        break;
       }
-      baseExist = true;
-    } else if (type == 1) {
-      for(var i=0; i<ing.length; i++) {
-        if(ing[i] == 0) {
-          ing[i] = id;
-          var src = document.getElementById('i'+(i+1));
-          src.appendChild(img);
-
-          var input = document.getElementsByName('i'+(i+1));
-          input[0].value = id;
-          console.log(id); 
-
-          break;
-        }
-      }
-    }
   }
-
-
+}
   function alc() {
     if (document.cookie == '') {
       d = new Date();
@@ -96,6 +80,7 @@ window.addEventListener('load', (event) => {
       }
       current = items[counter];
       current.classList.add('current');
+      updateHI(counter);
     }
     next.addEventListener('click', function(ev) {
       navigate(1);
